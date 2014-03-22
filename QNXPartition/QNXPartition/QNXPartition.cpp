fcf1ef06913b2564610de9230254778cbb3afc4c
Roman <HelloDearGrandma@gmail.com>
2014-03-22 14:03:05 +0300

    Теперь - без всяких там блять "подрепозиториев".
    Алёша: Ага, ага, да.

---
+++ QNXPartition/QNXPartition/QNXPartition.cpp
@@ -0,0 +1,111 @@
+// QNXPartition.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+//#include "WinBase.h"
+#include "windows.h"	// WinBase.h судя 
+#include "FileAPI.h"
+#include <iostream>
+
+using namespace std;
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	__int64 pos = 1000000;
+	TCHAR drivename[255];
+	TCHAR GUIDPath;
+	memset(drivename, 0, sizeof(drivename));
+//	swprintf_s(drivename, _T("\\\\.\\PhysicalDrive%d"), 0);
+	//swprintf_s(drivename, _T("\\\\.\\G:"), 0);
+
+	//Searching for a volumes
+	TCHAR tcDevName[256];
+	TCHAR tcVolumePathName[256];
+	DWORD dwResult = 0;
+	unsigned long size;
+	int counter = 1;
+	HANDLE hTempDev;
+	HANDLE hDev = FindFirstVolume(tcDevName, 256);
+	PARTITION_INFORMATION piDiskInfo;
+	do{
+		int len = wcslen(tcDevName);
+		tcDevName[len - 1] = L'\0';
+
+		wcout << _T("Volume GUID (") << counter << _T("): ") << tcDevName << endl;
+		GetVolumePathNamesForVolumeName(tcDevName, tcVolumePathName, 256, &size);
+		if (size == 0)
+		{
+			cout << "\tNot mounted" << endl;
+		}
+		else
+		{
+			wcout << _T("\tMounted on: ") << tcVolumePathName << endl;
+		}
+
+		//hTempDev = CreateFile(tcVolumePathName, GENERIC_READ, FILE_SHARE_READ | FILE_SHARE_WRITE,
+		//	NULL, OPEN_EXISTING, FILE_ATTRIBUTE_READONLY, NULL);
+		hTempDev = CreateFile(tcDevName, GENERIC_READ, FILE_SHARE_READ | FILE_SHARE_WRITE,
+							0, OPEN_EXISTING, NULL, NULL);
+		if (hTempDev == INVALID_HANDLE_VALUE)
+		{
+			cout << "\tERROR: CreateFile:" << GetLastError() << endl;
+			continue;
+		}
+
+		if (DeviceIoControl(hDev, IOCTL_DISK_GET_PARTITION_INFO_EX, NULL, 0,
+			&piDiskInfo, sizeof(piDiskInfo), &dwResult, NULL))
+		{
+			cout << "\t" << piDiskInfo.PartitionType << endl;
+		}
+		else
+		{
+			cout << "\tERROR: DeviceIoConrtol: " << GetLastError() << endl;
+		}
+	} while (++counter, FindNextVolume(hDev, tcDevName, 256));
+
+	FindVolumeClose(hDev);
+
+	//Drive umounting
+
+	// Drive opening
+	//HANDLE hDrive = CreateFile(drivename,
+	//	GENERIC_READ | GENERIC_WRITE,
+	//	FILE_SHARE_READ | FILE_SHARE_WRITE,
+	//	0,
+	//	OPEN_EXISTING,
+	//	NULL,
+	//	NULL);
+	//if (hDrive == INVALID_HANDLE_VALUE){
+	//	cout << "Fail to open disk;" << endl;
+	//	cin.get();
+	//	return 0;
+	//}
+	//cout << "Drive is opened;" << endl;
+	//
+	//const int bufSize = 6;
+	//char buf[bufSize];
+	//memset(buf, 0, bufSize);
+
+	//// Setting file pointer position
+	//LARGE_INTEGER n64Pos;
+	//n64Pos.QuadPart = 4;
+	//DWORD nRet = SetFilePointer(hDrive, n64Pos.LowPart, &n64Pos.HighPart, FILE_BEGIN);
+	//cout << "nRet: " << nRet << endl;
+
+	//// Reading data from drive
+	//DWORD dwBytesRead = 0;
+	//if (ReadFile(drivename, buf, 5, &dwBytesRead, NULL)){
+	//	buf[dwBytesRead] = '\0';
+	//	cout << "Bytes read: " << dwBytesRead << endl
+	//		<< "Bytes: " << buf << endl;
+	//}
+	//else{
+	//	DWORD err = GetLastError();
+	//	cout << "Bytes read: " << dwBytesRead << endl;
+	//	cout << "Error code: " << err << endl;
+	//}
+
+	cin.get();
+	return 0;
+}
+
---
+++ QNXPartition/QNXPartition/ReadMe.txt
@@ -0,0 +1,40 @@
+========================================================================
+    CONSOLE APPLICATION : QNXPartition Project Overview
+========================================================================
+
+AppWizard has created this QNXPartition application for you.
+
+This file contains a summary of what you will find in each of the files that
+make up your QNXPartition application.
+
+
+QNXPartition.vcxproj
+    This is the main project file for VC++ projects generated using an Application Wizard.
+    It contains information about the version of Visual C++ that generated the file, and
+    information about the platforms, configurations, and project features selected with the
+    Application Wizard.
+
+QNXPartition.vcxproj.filters
+    This is the filters file for VC++ projects generated using an Application Wizard. 
+    It contains information about the association between the files in your project 
+    and the filters. This association is used in the IDE to show grouping of files with
+    similar extensions under a specific node (for e.g. ".cpp" files are associated with the
+    "Source Files" filter).
+
+QNXPartition.cpp
+    This is the main application source file.
+
+/////////////////////////////////////////////////////////////////////////////
+Other standard files:
+
+StdAfx.h, StdAfx.cpp
+    These files are used to build a precompiled header (PCH) file
+    named QNXPartition.pch and a precompiled types file named StdAfx.obj.
+
+/////////////////////////////////////////////////////////////////////////////
+Other notes:
+
+AppWizard uses "TODO:" comments to indicate parts of the source code you
+should add to or customize.
+
+/////////////////////////////////////////////////////////////////////////////
